ffaaa4c4ded398621d507bdf3f2979f616fa75ce
wllnut <151087615+wllnut@users.noreply.github.com>
2024-11-28 15:58:17 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,71 +1,88 @@
-#!/bin/sh
+#!/bin/bash
 
+# Ensure the script is running with sudo privileges
+if [[ $(id -u) -ne 0 ]]; then
+  echo "Please run this script as root or with sudo privileges."
+  exit 1
+fi
+
+# Update system and install necessary packages
+echo "Updating system and installing necessary packages (git, flatpak)..."
+pacman -Syu --noconfirm git flatpak
+
+# Setup home directory structure
+echo "Setting up home directory..."
+mkdir -p ~/Code ~/Desktop ~/Documents ~/Downloads ~/Media/{music,pictures,video}
+
+# Clone the dwm repository
+echo "Cloning dotfile repository..."
 cd ~
-sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm git
-
-# Setup home directory
-mkdir Code
-mkdir Desktop
-mkdir Documents
-mkdir Downloads
-mkdir Media
-
-mkdir .config
-
-cd Media
-mkdir music
-mkdir pictures
-mkdir video
-mkdir .hidden
-
-cd ~
-
-# Clone github repo
 git clone https://github.com/wllnut/dwm
 cd dwm
 
-# Install packages from pacman_packages.list
-sudo pacman -S --noconfirm - < pacman_packages.list
+# Install packages from pacman_packages.list if it exists
+if [ -f pacman_packages.list ]; then
+  echo "Installing packages from pacman_packages.list..."
+  pacman -S --noconfirm - < pacman_packages.list
+else
+  echo "pacman_packages.list not found, skipping pacman package installation."
+fi
 
-# Install yay from aur
+# Install yay from the AUR if yay is not already installed
 cd ~
+echo "Installing yay from the AUR..."
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
-
-# Remove yay directory
 cd ~
 rm -rf yay
 
-# Install flatpaks
-cd dwm
-cat flatpak_packages.list | xargs flatpak install -y
-cd ~
+# Install Flatpak packages from flatpak_packages.list if it exists
+if [ -f flatpak_packages.list ]; then
+  echo "Installing Flatpak packages from flatpak_packages.list..."
+  flatpak install -y < flatpak_packages.list
+else
+  echo "flatpak_packages.list not found, skipping Flatpak package installation."
+fi
 
-# Copy config files
-cd dwm
-cp -r .config ~/.config
-cp -r .oh-my-zsh ~/.oh-my-zsh
-cp -r wallpaper ~/Media/pictures/.wallpaper
-cp .xinitrc ~/.xinitrc
-cp .zsh_history ~/.zsh_history
-cp .zshrc ~/.zshrc
+# Function to copy files if they exist
+copy_file_if_exists() {
+  if [ -f "$1" ]; then
+    cp "$1" "$2"
+  else
+    echo "$1 not found, skipping."
+  fi
+}
+
+# Copy configuration files
+echo "Copying configuration files..."
+for file in .xinitrc .zshrc .zsh_history; do
+  [ ! -f ~/$file ] && cp dwm/$file ~/$file
+done
+
+copy_file_if_exists "dwm/.oh-my-zsh" "~/.oh-my-zsh"
+copy_file_if_exists "dwm/.config" "~/.config"
+
+# Copy wallpaper if it exists
+copy_file_if_exists "dwm/wallpaper" "~/Media/pictures/.wallpaper"
 
 # Copy code files
-cp -r Code ~/Code
+echo "Copying code files..."
+cp -r dwm/Code ~/Code
 
 # Install suckless programs
-cd ~/.config/Suckless/dwm/ && sudo make clean install
+echo "Installing Suckless programs..."
+cd ~/.config/Suckless/dwm && sudo make clean install
 cd ~/.config/Suckless/dmenu && sudo make clean install
 cd ~/.config/Suckless/slock && sudo make clean install
 cd ~/.config/Suckless/slstatus && sudo make clean install
 
-# Start XORG session
-startx
+# Set the wallpaper if the file exists
+if [ -f ~/Media/pictures/.wallpaper/wallpaper.png ]; then
+  echo "Setting wallpaper..."
+  feh --bg-scale ~/Media/pictures/.wallpaper/wallpaper.png
+else
+  echo "Wallpaper not found, skipping wallpaper setting."
+fi
 
-# Set wallpaper
-feh --bg-scale ~/Media/pictures/.wallpaper/wallpaper.png
-
-# Reboot
-reboot
+echo "Installation complete. You may want to manually start X using 'startx' or reboot your system."
